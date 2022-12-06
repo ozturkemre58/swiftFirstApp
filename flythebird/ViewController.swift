c5da505f97e7ac7aab33308d900ef81faa6e5e07
ozturkemre58 <emreozturk.5874@gmail.com>
2022-12-06 15:58:05 +0300

    Initial Commit

---
+++ flythebird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flythebird
+//
+//  Created by Emre Ozturk on 6.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
